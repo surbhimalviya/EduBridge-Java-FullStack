651d4e999bc145ab3584729050261fad4a1583ac
Surbhi Malviya <83579458+surbhimalviya@users.noreply.github.com>
2021-08-02 21:53:10 +0530

    Add files via upload

---
+++ Assignment/Java/Basic Programs/Add.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Add
+{
+public static void main(String s[])
+{
+	Scanner sc=new Scanner(System.in);
+	System.out.print("enter first number :");
+	int x=sc.nextInt();
+	
+	System.out.print("enter second number :");
+	int y=sc.nextInt();
+	
+	int z=x+y;
+	System.out.print(z);
+}
+}
