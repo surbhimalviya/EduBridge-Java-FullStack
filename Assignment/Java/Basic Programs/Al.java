cc1ebf97bf38ba04947f4fa2d2d87e7099262356
Surbhi Malviya <83579458+surbhimalviya@users.noreply.github.com>
2021-09-01 11:46:24 +0530

    Add files via upload

---
+++ Assignment/Java/Basic Programs/Al.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Al
+{
+	public static void main(String args[])
+	{
+		ArrayList<String> alist=new ArrayList<String>();
+		alist.add("Surbhi");
+		alist.add("Suman");
+		alist.add("Sejal");
+		alist.add("Simmi");
+		alist.add("Sumu");
+		for(String s: alist)
+		System.out.println(s+ " ");
+		}
+}
---
+++ Assignment/Java/Basic Programs/Ascending.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Ascending
+{
+public static void main(String args[])
+{
+	int x,y,z;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the value x , y , z ");
+	x=sc.nextInt();
+	y=sc.nextInt();
+	z=sc.nextInt();
+	if (x>=y && y>=z)
+	{
+		System.out.println("order"+z+"  "+y+"  "+x);
+	}
+	else if (y>=x &&z>=y)
+	{
+		System.out.println("order"+x+" "+y+"  "+z);
+	}
+	
+	else if (y>=x && x>=z)
+	{
+		System.out.println("order"+z+"  "+x+"  "+y);
+	}
+	else if (z>=x && x>=y)
+	{
+		System.out.println("order"+y+"  "+x+"  "+z);
+	}
+	else 
+	{
+	System.out.println("order"+y+"  "+z+"  "+x);
+	}	
+}
+}
